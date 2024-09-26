5979879ccd046a3e5f30e8639aa6db84e420e946
Vaishali Thakkar <me.vaishalithakkar@gmail.com>
2024-09-26 14:20:45 +0200

    Add the test for sev
    Signed-off-by: Vaishali Thakkar <me.vaishalithakkar@gmail.com>

---
+++ tests/sev/main.c
@@ -0,0 +1,28 @@
+/**
+ * @file tests/sev/main.c
+ * @ref test-sev
+ *
+ * @page test-sev sev
+ *
+ * @todo Docs for test-sev
+ *
+ * @see tests/sev/main.c
+ */
+#include <xtf.h>
+
+const char test_title[] = "Test sev";
+
+void test_main(void)
+{
+    xtf_success(NULL);
+}
+
+/*
+ * Local variables:
+ * mode: C
+ * c-file-style: "BSD"
+ * c-basic-offset: 4
+ * tab-width: 4
+ * indent-tabs-mode: nil
+ * End:
+ */
