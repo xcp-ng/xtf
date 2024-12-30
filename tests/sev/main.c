2ad66b17920e7a6f1359e7fe9d939888003e7863
Teddy Astie <teddy.astie@outlook.fr>
2024-12-30 11:39:53 +0100

    Add demo content

--- tests/sev/main.c
+++ tests/sev/main.c
@@ -9,11 +9,22 @@
  * @see tests/sev/main.c
  */
 #include <xtf.h>
+#include <xtf/hypercall.h>
+#include <arch/sev.h>
 
 const char test_title[] = "Test sev";
 
+const char *secret_data = "This is some secret data";
+
 void test_main(void)
 {
+    printk("(GUEST): C-bit mask: %"PRIx64"\n", c_bit_mask());
+
+    printk("(GUEST): Secret data at %p\n", secret_data);
+    printk("(GUEST): Secret data is: %s\n", secret_data);
+
+    HYPERCALL2(long, __HYPERVISOR_sev_hox_demo_op, secret_data, strlen(secret_data));
+
     xtf_success(NULL);
 }
 
